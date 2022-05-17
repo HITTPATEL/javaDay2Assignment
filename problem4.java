7b8444521fbeedf703e448cbc8be46df4f848325
Hitendra Verma <hiten200verma@gmail.com>
2022-05-17 22:36:06 +0530

    problem 4 added

---
+++ problem4.java
@@ -0,0 +1,15 @@
+class P4{
+  public static void main(String[] args){
+       int ones=2;
+       int twos=3;
+       int threes=4;
+       int fours=5;   
+       int sixes=6;
+
+
+System.out.println(ones*1+twos*2+threes*3+fours*4+sixes*6);
+}
+
+
+
+}
